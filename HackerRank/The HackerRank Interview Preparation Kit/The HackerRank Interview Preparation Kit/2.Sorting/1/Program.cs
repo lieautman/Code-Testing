5bb2d5d1e04a76faa42645758efd81972aecb8d9
Cristi Dumitrescu <dumi_cristi@yahoo.com>
2024-10-08 15:15:30 +0300

    add project

---
+++ HackerRank/The HackerRank Interview Preparation Kit/The HackerRank Interview Preparation Kit/2.Sorting/1/Program.cs
@@ -0,0 +1,85 @@
+﻿/*using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    public class Player
+    {
+        public string name { get; set; }
+        public int score { get; set; }
+        public Player(string name, int score)
+        {
+            this.name = name;
+            this.score = score;
+        }
+        public override string ToString()
+        {
+            return name + " " + score;
+        }
+    }
+
+    public interface Comparator<T>
+    {
+        public int compare(T a, T b);
+    }
+
+    public class Checker : Comparator<Player>
+    {
+        public int compare(Player a, Player b)
+        {
+            if(a.score < b.score)
+            {
+                return -1;
+            }
+            if(a.score > b.score)
+            {
+                return 1;
+            }
+            if (String.Compare(a.name,b.name)>0)
+            {
+                return -1;
+            }
+            if (String.Compare(a.name, b.name) < 0)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+
+    static void Main(String[] args)
+    {
+        *//* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution *//*
+        int noOfPlayers = Int32.Parse(Console.ReadLine());
+        List<Player> listPlayer = new List<Player>();
+        for(int i = 0; i < noOfPlayers; i++)
+        {
+            string nameAndScore = Console.ReadLine();
+            int indexOfSpace = nameAndScore.IndexOf(" ");
+            string name = nameAndScore.Substring(0, indexOfSpace);
+            int score = Int32.Parse(nameAndScore.Substring(indexOfSpace, nameAndScore.Length - indexOfSpace));
+            Player player = new Player(name, score);
+            listPlayer.Add(player);
+        }
+        Checker c = new Checker();
+        for (int i= 0;i< listPlayer.Count()-1;i++)
+        {
+            for(int j=i;j< listPlayer.Count(); j++)
+            {
+                if (c.compare(listPlayer[i], listPlayer[j]) == -1)
+                {
+                    Player temp = listPlayer[i];
+                    listPlayer[i] = listPlayer[j];
+                    listPlayer[j] = temp;
+                }
+            }
+        }
+
+        foreach(Player p in listPlayer)
+        {
+            Console.WriteLine(p.ToString());
+        }
+
+    }
+}*/
---
+++ HackerRank/The HackerRank Interview Preparation Kit/The HackerRank Interview Preparation Kit/2.Sorting/2/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'activityNotifications' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY expenditure
+     *  2. INTEGER d
+     */
+
+    public static int activityNotifications(List<int> expenditure, int d)
+    {
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int n = Convert.ToInt32(firstMultipleInput[0]);
+
+        int d = Convert.ToInt32(firstMultipleInput[1]);
+
+        List<int> expenditure = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(expenditureTemp => Convert.ToInt32(expenditureTemp)).ToList();
+
+        int result = Result.activityNotifications(expenditure, d);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
